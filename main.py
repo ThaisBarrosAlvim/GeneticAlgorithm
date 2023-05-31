099ac531bfdcfdd81da101813f5d72d92e028135
ThaisAlvim <thaisbarrosalvim@gmail.com>
2023-05-31 18:07:36 -0300

    finish reading file to flights list

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ def min_to_hour(minutes: int):
     return str(minutes // 60) + ':' + str(minutes % 60)
 
 
-def fitness(organism, target_time):
-    return 1 / (organism.price + 1)
-
-
 class Flight:
     def __init__(self, index: int, dep_airport: str, arr_airport: str, dep_time: int, arr_time: int, price: int):
         self.id = index
@@ -29,25 +25,37 @@ class Flight:
         return min_to_hour(self.arr_time)
 
     def __str__(self):
-        return f'{self.dep_airport} {self.arr_airport} {self.dep_time_show()} {self.arr_time_show()} {self.price}'
+        return f'Flight({self.id}): ' \
+               f'{self.dep_airport} {self.arr_airport} {self.dep_time_show()} {self.arr_time_show()} {self.price}'
+
+    def __repr__(self):
+        return str(self)
+
+
+class Organism:
+    def __init__(self, flights: list):
+        self.flights = flights
+        self.fitness = 0
+
+    def __str__(self):
+        return f'Organism: {self.fitness}'
 
     def __repr__(self):
         return str(self)
 
 
 def read_file(path):
-    # FCO,LIS,6:19,8:13,239
-    population = []
+    flights = []
     df = pd.read_csv(path, sep=',', header=None)
     for index, row in enumerate(df.iterrows()):
-        population.append(Flight(index, row[1][0], row[1][1], hour_to_min(row[1][2]), hour_to_min(row[1][3]), row[1][4]))
-    return population
+        # FCO,LIS,6:19,8:13,239
+        flights.append(Flight(index, row[1][0], row[1][1], hour_to_min(row[1][2]), hour_to_min(row[1][3]), row[1][4]))
+    return flights
 
 
 def main():
-    population = read_file('flights.txt')
-    for organism in population:
-        print(organism)
+    flights = read_file('flights.txt')
+    # TODO continue, create logic of first population, index jump to 20 to change
 
 
 if __name__ == '__main__':
